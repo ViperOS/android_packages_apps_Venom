f6826df3da250aa92588fba72ddad6eadb4e2824
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-06-10 15:36:58 -0300

    Move heads up settings to Venom [2/2]

---
+++ src/com/viper/venom/fragments/NotificationsSettings.java
@@ -0,0 +1,54 @@
+package com.viper.venom.fragments;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Configuration;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.text.format.DateFormat;
+import android.provider.Settings;
+import android.os.UserHandle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+
+public class NotificationsSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.notifications_settings);
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        return false;
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.VENOM;
+    }
+    
+}
