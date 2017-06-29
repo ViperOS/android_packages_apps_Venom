9bd53bb111070ada7120b718e1ae1516e78f20ca
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-06-29 11:20:39 -0300

    Ticker: Remove extra preferences

--- src/com/viper/venom/fragments/NotificationsSettings.java
+++ src/com/viper/venom/fragments/NotificationsSettings.java
@@ -28,7 +28,6 @@ import android.widget.Toast;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 
 public class NotificationsSettings extends SettingsPreferenceFragment
@@ -38,17 +37,11 @@ public class NotificationsSettings extends SettingsPreferenceFragment
     private static final String PREF_HEADS_UP_SNOOZE_TIME = "heads_up_snooze_time";
 
     private static final String PREF_SHOW_TICKER = "status_bar_show_ticker";
-    private static final String PREF_TEXT_COLOR = "status_bar_ticker_text_color";
-    private static final String PREF_ICON_COLOR = "status_bar_ticker_icon_color";
-    private static final String PREF_TICKER_RESTORE_DEFAULTS = "ticker_restore_defaults";
 
     private ListPreference mHeadsUpTimeOut;
     private ListPreference mHeadsUpSnoozeTime;
 
     private ListPreference mShowTicker;
-    private ColorPickerPreference mTextColor;
-    private ColorPickerPreference mIconColor;
-    private Preference mTickerDefaults;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -65,24 +58,6 @@ public class NotificationsSettings extends SettingsPreferenceFragment
         mShowTicker.setValue(String.valueOf(tickerMode));
         mShowTicker.setSummary(mShowTicker.getEntry());
         mShowTicker.setOnPreferenceChangeListener(this);
-
-        mTextColor = (ColorPickerPreference) prefSet.findPreference(PREF_TEXT_COLOR);
-        mTextColor.setOnPreferenceChangeListener(this);
-        int textColor = Settings.System.getInt(resolver,
-            Settings.System.STATUS_BAR_TICKER_TEXT_COLOR, 0xffffffff);
-        String textHexColor = String.format("#%08x", (0xffffffff & textColor));
-        mTextColor.setSummary(textHexColor);
-        mTextColor.setNewPreviewColor(textColor);
-
-        mIconColor = (ColorPickerPreference) prefSet.findPreference(PREF_ICON_COLOR);
-        mIconColor.setOnPreferenceChangeListener(this);
-        int iconColor = Settings.System.getInt(resolver,
-            Settings.System.STATUS_BAR_TICKER_ICON_COLOR, 0xffffffff);
-        String iconHexColor = String.format("#%08x", (0xffffffff & iconColor));
-        mIconColor.setSummary(iconHexColor);
-        mIconColor.setNewPreviewColor(iconColor);
-
-        mTickerDefaults = prefSet.findPreference(PREF_TICKER_RESTORE_DEFAULTS);
 
         Resources systemUiResources;
         try {
@@ -135,53 +110,8 @@ public class NotificationsSettings extends SettingsPreferenceFragment
             int index = mShowTicker.findIndexOfValue((String) newValue);
             mShowTicker.setSummary(mShowTicker.getEntries()[index]);
             return true;
-        } else if (preference == mTextColor) {
-            String hex = ColorPickerPreference.convertToARGB(
-                Integer.valueOf(String.valueOf(newValue)));
-            preference.setSummary(hex);
-            int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
-                Settings.System.STATUS_BAR_TICKER_TEXT_COLOR, intHex);
-            return true;
-        } else if (preference == mIconColor) {
-            String hex = ColorPickerPreference.convertToARGB(
-                Integer.valueOf(String.valueOf(newValue)));
-            preference.setSummary(hex);
-            int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
-                Settings.System.STATUS_BAR_TICKER_ICON_COLOR, intHex);
-            return true;
         }
         return false;
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(final Preference preference) {
-        final ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mTickerDefaults) {
-            int intColor;
-            String hexColor;
-
-            Settings.System.putInt(resolver,
-                    Settings.System.STATUS_BAR_TICKER_TEXT_COLOR, 0xffffffff);
-            intColor = Settings.System.getInt(resolver,
-                    Settings.System.STATUS_BAR_TICKER_TEXT_COLOR, 0xffffffff);
-            hexColor = String.format("#%08x", (0xffffffff & intColor));
-            mTextColor.setSummary(hexColor);
-            mTextColor.setNewPreviewColor(intColor);
-
-            Settings.System.putInt(resolver,
-                    Settings.System.STATUS_BAR_TICKER_ICON_COLOR, 0xffffffff);
-            intColor = Settings.System.getInt(resolver,
-                    Settings.System.STATUS_BAR_TICKER_ICON_COLOR, 0xffffffff);
-            hexColor = String.format("#%08x", (0xffffffff & intColor));
-            mIconColor.setSummary(hexColor);
-            mIconColor.setNewPreviewColor(intColor);
-
-            Toast.makeText(getActivity(), R.string.values_restored_title,
-                    Toast.LENGTH_LONG).show();
-        }
-        return super.onPreferenceTreeClick(preference);
     }
 
     private void updateHeadsUpTimeOutSummary(int value) {
