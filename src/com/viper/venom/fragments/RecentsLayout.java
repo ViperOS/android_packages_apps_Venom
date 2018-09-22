3f503c02bec20a99536af903409efc3868c18364
maxwen <max.weninger@gmail.com>
2018-09-22 09:57:07 -0300

    [2/2] Venom: featureize ro.recents.grid and Go Layout
    thanks google for coding like a 1st grader by adding public
    member variables and making this more work then needed
    
    edit: Update the code for our systemui reboot util and add Go recent style as a possible feature

--- src/com/viper/venom/fragments/RecentsLayout.java
+++ src/com/viper/venom/fragments/RecentsLayout.java
@@ -16,47 +16,70 @@
 
 package com.viper.venom.fragments;
 
-
-import android.app.ActivityManagerNative;
+import android.app.Activity;
 import android.content.Context;
 import android.content.ContentResolver;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
-import android.support.v7.preference.Preference;
 import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
-import android.util.Log;
-import android.view.WindowManagerGlobal;
-import android.view.IWindowManager;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import java.util.Locale;
-import android.text.TextUtils;
-import android.view.View;
-
 import com.android.settings.R;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.settings.Utils;
 
-public class RecentsLayout extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+import com.viper.venom.utils.DeviceUtils;
+
+public class RecentsLayout extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+
+    private ListPreference mRecentsType;
+    private static final String RECENTS_TYPE = "recents_layout_style";
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.recents_layout);
 
-        final ContentResolver resolver = getActivity().getContentResolver();
+        ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
+
+        // recents type
+        mRecentsType = (ListPreference) findPreference(RECENTS_TYPE);
+        int style = Settings.System.getIntForUser(resolver,
+                Settings.System.RECENTS_LAYOUT_STYLE, 0, UserHandle.USER_CURRENT);
+        mRecentsType.setValue(String.valueOf(style));
+        mRecentsType.setSummary(mRecentsType.getEntry());
+        mRecentsType.setOnPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mRecentsType) {
+            int style = Integer.valueOf((String) objValue);
+            int index = mRecentsType.findIndexOfValue((String) objValue);
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_LAYOUT_STYLE, style, UserHandle.USER_CURRENT);
+            mRecentsType.setSummary(mRecentsType.getEntries()[index]);
+            DeviceUtils.restartSystemUi(getContext());
+        return true;
+        }
+    return false;
 
     }
 
@@ -65,13 +88,4 @@ public class RecentsLayout extends SettingsPreferenceFragment implements OnPrefe
         return MetricsEvent.VENOM;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        return false;
-    }
 }
