f2e8dac3a01ca69cd4f1d62ae9dec847ddfd8a4a
maxwen <max.weninger@gmail.com>
2018-09-23 12:24:58 -0300

    Redo advanced reboot and power menu settings

--- src/com/viper/venom/fragments/PowerMenuSettings.java
+++ src/com/viper/venom/fragments/PowerMenuSettings.java
@@ -16,56 +16,62 @@
 
 package com.viper.venom.fragments;
 
-
-import android.app.ActivityManagerNative;
-import android.content.Context;
 import android.content.ContentResolver;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.res.Resources;
+import android.content.Context;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.os.RemoteException;
-import android.os.ServiceManager;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.SearchIndexableResource;
 import android.provider.Settings;
-import android.util.Log;
-import android.view.WindowManagerGlobal;
-import android.view.IWindowManager;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import java.util.Locale;
-import android.text.TextUtils;
-import android.view.View;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.settings.Utils;
 
-import lineageos.providers.LineageSettings;
+import java.util.List;
+import java.util.ArrayList;
 
-public class PowerMenuSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+import com.viper.venom.preference.SecureSettingSwitchPreference;
+
+public class PowerMenuSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "PowerMenuSettings";
+
+    private SecureSettingSwitchPreference mAdvancedReboot;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.powermenu_settings);
 
-        final ContentResolver resolver = getActivity().getContentResolver();
-        final PreferenceScreen prefSet = getPreferenceScreen();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final ContentResolver contentResolver = getContext().getContentResolver();
 
+        mAdvancedReboot = (SecureSettingSwitchPreference)
+                findPreference(Settings.Secure.ADVANCED_REBOOT);
+        mAdvancedReboot.setOnPreferenceChangeListener(this);
     }
 
-    public static void reset(Context mContext) {
-        ContentResolver resolver = mContext.getContentResolver();
-        LineageSettings.Secure.putIntForUser(resolver,
-                LineageSettings.Secure.ADVANCED_REBOOT, 1, UserHandle.USER_CURRENT);
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+         boolean result = false;
+         if (preference instanceof SecureSettingSwitchPreference) {
+             if (preference == mAdvancedReboot) {
+                boolean value = (Boolean) objValue;
+                Settings.Secure.putInt(getContentResolver(), Settings.Secure.ADVANCED_REBOOT,
+                        value ? 1:0);
+                Settings.Secure.putInt(getContentResolver(), Settings.Secure.GLOBAL_ACTION_DNAA,
+                        value ? 1:0);
+             }
+             return true;
+         }
+         return result;
     }
 
     @Override
@@ -73,13 +79,4 @@ public class PowerMenuSettings extends SettingsPreferenceFragment implements OnP
         return MetricsEvent.VENOM;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        return false;
-    }
 }
