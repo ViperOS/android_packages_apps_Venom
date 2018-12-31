0a9dfbe21b150afe2bf9ec3c865ab857167ae002
ezio84 <brabus84@gmail.com>
2018-12-31 17:29:33 -0200

    Battery icon style: improve text/hidden switch [2/2]
    if the set style is text or hidden, don't allow
    percentage option (inside/outside icon) to break it

--- src/com/viper/venom/fragments/BatterysSettings.java
+++ src/com/viper/venom/fragments/BatterysSettings.java
@@ -31,21 +31,56 @@ import com.android.settings.SettingsPreferenceFragment;
 public class BatterysSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private ListPreference mBatteryIconStyle;
+    private ListPreference mBatteryPercentage;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.batterys_settings);
         final ContentResolver resolver = getActivity().getContentResolver();
 
+        int batteryStyle = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0/*portrait*/,
+                UserHandle.USER_CURRENT);
+        mBatteryIconStyle = (ListPreference) findPreference("status_bar_battery_style");
+        mBatteryIconStyle.setValue(Integer.toString(batteryStyle));
+        mBatteryIconStyle.setOnPreferenceChangeListener(this);
+
+        int percentage = Settings.System.getIntForUser(resolver,
+                Settings.System.SHOW_BATTERY_PERCENT, 1,
+                UserHandle.USER_CURRENT);
+        mBatteryPercentage = (ListPreference) findPreference("status_bar_show_battery_percent");
+        mBatteryPercentage.setValue(Integer.toString(percentage));
+        mBatteryPercentage.setOnPreferenceChangeListener(this);
+        boolean hideForcePercentage =
+                batteryStyle == 5 || batteryStyle == 6; /*text or hidden style*/
+        mBatteryPercentage.setEnabled(!hideForcePercentage);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+       if (preference == mBatteryIconStyle) {
+            int value = Integer.valueOf((String) newValue);
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.STATUS_BAR_BATTERY_STYLE, value,
+                    UserHandle.USER_CURRENT);
+            boolean hideForcePercentage = value == 5 || value == 6;/*text or hidden style*/
+            mBatteryPercentage.setEnabled(!hideForcePercentage);
+            return true;
+        } else  if (preference == mBatteryPercentage) {
+            int value = Integer.valueOf((String) newValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.SHOW_BATTERY_PERCENT, value,
+                    UserHandle.USER_CURRENT);
+            boolean hideForcePercentage = value == 5 || value == 6;/*text or hidden style*/
+            mBatteryPercentage.setEnabled(!hideForcePercentage);
+            return true;
+        }
+        return false;
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.VENOM;
-    }
-
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-
-        return false;
     }
 }
