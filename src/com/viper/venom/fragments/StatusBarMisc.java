1fd4d13208123e768387fd539cfb7ac54a6728cb
AgentFabulous <kshitijgm@gmail.com>
2019-01-25 11:59:13 -0200

    Allow using framework values for rounded corners

--- src/com/viper/venom/fragments/StatusBarMisc.java
+++ src/com/viper/venom/fragments/StatusBarMisc.java
@@ -16,14 +16,26 @@
 
 package com.viper.venom.fragments;
 
-import android.content.Context;
+import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
- import android.support.v14.preference.SwitchPreference;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -32,34 +44,66 @@ import com.android.internal.logging.nano.MetricsProto;
 
 import com.viper.venom.support.preferences.CustomSeekBarPreference;
 
-public class StatusBarMisc extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
+public class StatusBarMisc extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
-    private static final String SYSUI_ROUNDED_CONTENT_PADDING = "sysui_rounded_content_padding";
     private static final String SYSUI_ROUNDED_SIZE = "sysui_rounded_size";
+    private static final String SYSUI_ROUNDED_CONTENT_PADDING = "sysui_rounded_content_padding";
+    private static final String SYSUI_STATUS_BAR_PADDING = "sysui_status_bar_padding";
+    private static final String SYSUI_ROUNDED_FWVALS = "sysui_rounded_fwvals";
     private static final String KEY_STATUS_BAR_LOGO = "status_bar_logo";
 
-    private CustomSeekBarPreference mContentPadding;
     private CustomSeekBarPreference mCornerRadius;
+    private CustomSeekBarPreference mContentPadding;
+    private CustomSeekBarPreference mSBPadding;
+    private SwitchPreference mRoundedFwvals;
     private SwitchPreference mShowVpLogo;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.status_bar_misc);
-        PreferenceScreen prefSet = getPreferenceScreen();
-        ContentResolver resolver = getActivity().getContentResolver();
 
-        mContentPadding = (CustomSeekBarPreference) findPreference(SYSUI_ROUNDED_CONTENT_PADDING);
-        int contentPadding = Settings.Secure.getInt(resolver,
-                Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING, 1);
-                mContentPadding.setValue(contentPadding / 1);
-                mContentPadding.setOnPreferenceChangeListener(this);
+        Resources res = null;
+        Context ctx = getContext();
+        float density = Resources.getSystem().getDisplayMetrics().density;
 
+        try {
+            res = ctx.getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Rounded Corner Radius
         mCornerRadius = (CustomSeekBarPreference) findPreference(SYSUI_ROUNDED_SIZE);
-        int cornerRadius = Settings.Secure.getInt(resolver,
-                Settings.Secure.SYSUI_ROUNDED_SIZE, 1);
-                mCornerRadius.setValue(cornerRadius / 1);
-                mCornerRadius.setOnPreferenceChangeListener(this);
+        int resourceIdRadius = res.getIdentifier("com.android.systemui:dimen/rounded_corner_radius", null, null);
+        int cornerRadius = Settings.Secure.getIntForUser(ctx.getContentResolver(), Settings.Secure.SYSUI_ROUNDED_SIZE,
+                (int) (res.getDimension(resourceIdRadius) / density), UserHandle.USER_CURRENT);
+        mCornerRadius.setValue(cornerRadius);
+        mCornerRadius.setOnPreferenceChangeListener(this);
+
+        // Rounded Content Padding
+        mContentPadding = (CustomSeekBarPreference) findPreference(SYSUI_ROUNDED_CONTENT_PADDING);
+        int resourceIdPadding = res.getIdentifier("com.android.systemui:dimen/rounded_corner_content_padding", null,
+                null);
+        int contentPadding = Settings.Secure.getIntForUser(ctx.getContentResolver(),
+                Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING,
+                (int) (res.getDimension(resourceIdPadding) / density), UserHandle.USER_CURRENT);
+        mContentPadding.setValue(contentPadding);
+        mContentPadding.setOnPreferenceChangeListener(this);
+
+        // Status Bar Content Padding
+        mSBPadding = (CustomSeekBarPreference) findPreference(SYSUI_STATUS_BAR_PADDING);
+        int resourceIdSBPadding = res.getIdentifier("com.android.systemui:dimen/status_bar_extra_padding", null,
+                null);
+        int sbPadding = Settings.Secure.getIntForUser(ctx.getContentResolver(),
+                Settings.Secure.SYSUI_STATUS_BAR_PADDING,
+                (int) (res.getDimension(resourceIdSBPadding) / density), UserHandle.USER_CURRENT);
+        mSBPadding.setValue(sbPadding);
+        mSBPadding.setOnPreferenceChangeListener(this);
+
+        // Rounded use Framework Values
+        mRoundedFwvals = (SwitchPreference) findPreference(SYSUI_ROUNDED_FWVALS);
+        mRoundedFwvals.setOnPreferenceChangeListener(this);
 
         mShowVpLogo = (SwitchPreference) findPreference(KEY_STATUS_BAR_LOGO);
         mShowVpLogo.setChecked((Settings.System.getInt(getContentResolver(),
@@ -68,27 +112,52 @@ public class StatusBarMisc extends SettingsPreferenceFragment implements Prefere
 
     }
 
+    private void restoreCorners() {
+        Resources res = null;
+        float density = Resources.getSystem().getDisplayMetrics().density;
+
+        try {
+            res = getContext().getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        int resourceIdRadius = res.getIdentifier("com.android.systemui:dimen/rounded_corner_radius", null, null);
+        int resourceIdPadding = res.getIdentifier("com.android.systemui:dimen/rounded_corner_content_padding", null,
+                null);
+        int resourceIdSBPadding = res.getIdentifier("com.android.systemui:dimen/status_bar_extra_padding", null,
+                null);
+        mCornerRadius.setValue((int) (res.getDimension(resourceIdRadius) / density));
+        mContentPadding.setValue((int) (res.getDimension(resourceIdPadding) / density));
+        mSBPadding.setValue((int) (res.getDimension(resourceIdSBPadding) / density));
+    }
+
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mContentPadding) {
-            int value = (Integer) newValue;
-            Settings.Secure.putInt(getContentResolver(),
-                    Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING, value * 1);
-            return true;
-        } else if (preference == mCornerRadius) {
-            int value = (Integer) newValue;
-            Settings.Secure.putInt(getContentResolver(),
-                    Settings.Secure.SYSUI_ROUNDED_SIZE, value * 1);
-            return true;
+        if (preference == mCornerRadius) {
+            Settings.Secure.putIntForUser(getContext().getContentResolver(), Settings.Secure.SYSUI_ROUNDED_SIZE,
+                    (int) newValue, UserHandle.USER_CURRENT);
+        } else if (preference == mContentPadding) {
+            Settings.Secure.putIntForUser(getContext().getContentResolver(), Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING,
+                    (int) newValue, UserHandle.USER_CURRENT);
+        } else if (preference == mSBPadding) {
+            Settings.Secure.putIntForUser(getContext().getContentResolver(), Settings.Secure.SYSUI_STATUS_BAR_PADDING,
+                    (int) newValue, UserHandle.USER_CURRENT);
+        } else if (preference == mRoundedFwvals) {
+            restoreCorners();
         } else if  (preference == mShowVpLogo) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.STATUS_BAR_LOGO, value ? 1 : 0);
-            return true;
         }
-        return false;
-     }
+        return true;
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.SYSUI_ROUNDED_FWVALS, 1, UserHandle.USER_CURRENT);
+    }
 
     @Override
     public int getMetricsCategory() {
